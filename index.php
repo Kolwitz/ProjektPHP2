87467ed94f94e7c2f3f3c7d68cc8a3c7ddc5c2fa
Kolwitz <kolwitz@marmalade.de>
2013-02-12 17:50:48 +0100

    Stand heute 17:46

--- index.php
+++ index.php
@@ -2,11 +2,14 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>index</title>
     </head>
-    <body>
+          
         <?php
-         echo "erste Anfrage";
+         include 'bootstrap.php';
+         echo "test";
+          
+                   
         ?>
     </body>
 </html>
